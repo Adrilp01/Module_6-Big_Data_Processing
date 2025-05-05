c0bd8c6c0eef916c08b72d1c52fa319b871b2b05
Adrian Lopez Robles <Adrilopezrobles@gmail.com>
2025-05-05 14:38:34 +0200

    Update to English examn.scala

--- src/main/scala/examen/examen.scala
+++ src/main/scala/examen/examen.scala
@@ -1,66 +1,66 @@
-package examen
+package examn
 
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession, functions}
 
-object examen {
+object examn {
 
   //EJERCICIO 1
-  def ejercicio1(estudiantes: DataFrame)(implicit spark:SparkSession): DataFrame = {
+  def exercise1(students: DataFrame)(implicit spark:SparkSession): DataFrame = {
 
     import spark.implicits._
 
-    println("Imprimo esquema")
-    estudiantes.printSchema()
-    println("Imprimo calificaciones mayores a 8")
-    estudiantes.filter("Calificacion > 8").show()
-    println("Imprimo nombres ordenados por calificacion") //si quisiera mostrar solo los nombres tendía que quitar las calificaciones del select
-    val estudiantesorden = estudiantes.select("Nombre", "Calificacion").orderBy(col("Calificacion").desc)
-    println("Imprimo nuevo dataframe")
-    estudiantesorden.show()
-    estudiantesorden
+    println("Print schema")
+    students.printSchema()
+    println("Print marks greater than 8")
+    students.filter("Marks > 8").show()
+    println("print name sorted by mark.") //si quisiera mostrar solo los nombres tendía que quitar las calificaciones del select
+    val ordered_students = students.select("Name", "Marks").orderBy(col("Marks").desc)
+    println("Print new dataframe")
+    ordered_students.show()
+    ordered_students
   }
 
 
   //EJERCICIO 2
-  def ejercicio2(numeros: DataFrame)(implicit spark:SparkSession): DataFrame =  {
+  def exercise2(numbers: DataFrame)(implicit spark:SparkSession): DataFrame =  {
     import spark.implicits._
-    val ParImparUDF = udf((n: Int) => if (n % 2 == 0) s"$n es par" else s"$n es impar")
-    val newdf = numeros.withColumn("Par_Impar", ParImparUDF(numeros("Edad")))
+    val Even_oddUDF = udf((n: Int) => if (n % 2 == 0) s"$n is even" else s"$n is odd")
+    val newdf = numbers.withColumn("Even_Odd", Even_oddUDF(numbers("Age")))
     newdf.show()
     newdf
   }
 
   //EJERCICIO 3
-  def ejercicio3(estudiantes: DataFrame , calificaciones: DataFrame): DataFrame = {
-    val result = estudiantes.join(calificaciones, estudiantes("ID") === calificaciones("ID_estudiante"))
-    val medias = result.groupBy("ID", "Nombre").agg(avg("Calificacion").alias("Media"))
+  def exercise3(students: DataFrame , marks: DataFrame): DataFrame = {
+    val result = students.join(marks, students("ID") === marks("ID_student"))
+    val averages = result.groupBy("ID", "Name").agg(avg("Mark").alias("Average"))
     result.show()
     medias.show()
     medias
   }
 
   //EJERCICIO 4
-  def ejercicio4(palabras: List[String])(implicit spark:SparkSession): RDD[(String, Int)] = {
-    val rdd = spark.sparkContext.parallelize(palabras) //convierto la lista en rdd
-    val contar = rdd.map(a => (a, 1)).reduceByKey(_ + _)
-    contar.collect().foreach(println)
-    contar
+  def exercise4(words: List[String])(implicit spark:SparkSession): RDD[(String, Int)] = {
+    val rdd = spark.sparkContext.parallelize(words) //convert list into RDD
+    val count = rdd.map(a => (a, 1)).reduceByKey(_ + _)
+    count.collect().foreach(println)
+    count
   }
 
   //EJERCICIO 5
-  def ejercicio5(ventas: DataFrame)(implicit spark:SparkSession): DataFrame = {
+  def exercise5(sales: DataFrame)(implicit spark:SparkSession): DataFrame = {
     import spark.implicits._
-    val cantXprecio = ventas.withColumn("dineroXid", col("cantidad") * col("precio_unitario"))
+    val quantXprice = sales.withColumn("MoneyXid", col("cantidad") * col("precio_unitario"))
 
-    val totalXProducto = cantXprecio.groupBy("id_producto").agg(sum("dineroXid").alias("dineroXproducto"))
+    val totalXProduct = quantXprice.groupBy("id_producto").agg(sum("MoneyXid").alias("MoneyXproduct"))
 
     //val en_orden = totalXProducto.orderBy(desc("id_producto")) //lo ordeno para que quede mas claro el resultado
 
-    totalXProducto.show()
-    totalXProducto
+    totalXProduct.show()
+    totalXProduct
   }
 
 
